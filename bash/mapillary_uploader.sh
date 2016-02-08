577ccd8739e58fa29de278e05545d2488519f3b7
Thomas "moenk" Meier <moenk@geoclub.de>
2016-02-08 16:34:27 +0100

    crop to 16x9 option

--- bash/mapillary_uploader.sh
+++ bash/mapillary_uploader.sh
@@ -4,35 +4,64 @@
 #
 #	coder: moenkemt@geo.hu-berlin.de
 #
-#	purpose: delete duplicates from Garmin VIRB, build squences, 
+#	purpose: delete duplicates from Garmin VIRB, split image in 
+#	squences, crop 4x3 pictures to upper 16x9 part (75%),
 #	upload to mapillary sequences with at least 5 pictures
 #
 
-export MAPILLARY_DATA="/data/temp/baumi"
+export MAPILLARY_DATA="/data/temp/DCIM"
 export MAPILLARY_PATH="/root/mapillary_tools/python"
 export MAPILLARY_USERNAME="moenk"
 export MAPILLARY_PERMISSION_HASH="***"
 export MAPILLARY_SIGNATURE_HASH="***"
 
-for file in $MAPILLARY_DATA/*
-do
-	echo $file
-	$MAPILLARY_PATH/remove_duplicates.py -d 2 $file $file/dup
-	$MAPILLARY_PATH/sequence_split.py $file 120 500
-	rm -r $file/dup
-done
+function dupes {
+	for file in $MAPILLARY_DATA/*
+	do
+		echo dupes: "$file"
+		$MAPILLARY_PATH/remove_duplicates.py -d 2 $file $file/dup
+		rm -r $file/dup
+	done
+}
 
-shopt -s nocaseglob
-find $MAPILLARY_DATA/ -type d | sort | while read file
-do
-	num=`ls -l $file/*.jpg | wc -l`
-	if [ "$num" -gt "5" ]; then
-		if [ ! -d "$file/success" ]; then
-			echo "Upload now: $file ($num)"
-			yes | $MAPILLARY_PATH/upload_with_authentication.py $file
-		else
-			echo "Already done: $file"
+function sequencer {
+	for file in $MAPILLARY_DATA/*
+	do
+		echo sequencer: "$file"
+		$MAPILLARY_PATH/sequence_split.py $file 120 300
+	done
+}
+
+function resize {
+	for file in $(find $MAPILLARY_DATA -iname '*.jpg'); do 
+		echo resize to 16x9: "$file"
+		convert "$file" -gravity north -crop 100x75% +repage "$file"
+	done
+}
+
+
+function upload {
+	shopt -s nocaseglob
+	find $MAPILLARY_DATA/ -type d | sort | while read file
+	do
+		num=`ls -l $file/*.jpg | wc -l`
+		if [ "$num" -gt "5" ]; then
+			if [ ! -d "$file/success" ]; then
+				echo "Upload now: $file ($num)"
+				yes | $MAPILLARY_PATH/upload_with_authentication.py $file
+			else
+				echo "Already done: $file"
+			fi
 		fi
-	fi
-	rm $file/success/ -r
-done  
+		rm $file/success/ -r
+	done  
+}
+
+#
+# main
+#
+dupes
+sequencer
+resize
+upload
+ 
