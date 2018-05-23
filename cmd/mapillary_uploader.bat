4100377749faf4219bacf3f569566277f32d67be
Thomas "moenk" Meier <moenk@geoclub.de>
2018-05-23 22:00:02 +0200

    osc update

--- cmd/mapillary_uploader.bat
+++ cmd/mapillary_uploader.bat
@@ -8,23 +8,39 @@ rem		requires: 	image magick portable version extracted
 rem 				mapillary_tools from github extracted
 rem 				openstreetview\upload-scripts extracted
 rem					python 2.7 with pip install -r requirements (mapillary)
-rem					python 3.4 with pip install -r requirements (openstreetcam)
+rem					python 3.5 with pip install -r requirements (openstreetcam)
+rem
+rem		note:		important to use python version 2.7 and 3.5
 rem
 
-set MAPILLARY_USERNAME=moenk
+rem 	credentials
+set MAPILLARY_USERNAME=***
 set MAPILLARY_PERMISSION_HASH=***
 set MAPILLARY_SIGNATURE_HASH=***
 
 D:
 FOR /D %%d IN (d:\Mapillary\DCIM\???_VIRB) DO (
 	cd %%d
-    c:\python27\python.exe D:\Mapillary\mapillary_tools\python\remove_duplicates.py -a 180 -t 250 -v %%d
+    rem remove duplcates
+	c:\python27\python.exe D:\Mapillary\mapillary_tools\python\remove_duplicates.py -a 180 -t 250 -v %%d
     rmdir %%d\duplicates /s /q
-    FOR %%f IN (*.jpg) DO "\Mapillary\image_magick\convert.exe" -verbose "%%f" -normalize -quality 85 -resize 2048x "%%f"
+
+	rem resize and sequence
+    FOR %%f IN (*.jpg) DO "\Mapillary\image_magick\convert.exe" -verbose "%%f" -normalize -quality 85 -resize 1920x "%%f"
     c:\python27\python.exe D:\Mapillary\mapillary_tools\python\sequence_split.py %%d 120 300
-    FOR /D %%s IN (%%d\??) DO c:\python27\python.exe D:\Mapillary\mapillary_tools\python\upload_with_authentication.py %%s --auto_done
+
+	rem upload to mapillary
+    FOR /D %%s IN (%%d\??) DO (
+		del %%d\DONE
+		c:\python27\python.exe D:\Mapillary\mapillary_tools\python\upload_with_authentication.py %%s --auto_done
+	)
+
+	rem upload to openstreetview
 	cd \Mapillary\openstreetview\upload-scripts\upload_photos_by_exif
-	FOR /D %%s IN (%%d\??) DO c:\python34\python.exe upload_photos_by_exif.py -p %%s\success
+	FOR /D %%s IN (%%d\??) DO (
+		start c:\python35\python.exe upload_exif.py -t 1 -p %%s\success
+	)
 )
 
+rem and done
 pause
