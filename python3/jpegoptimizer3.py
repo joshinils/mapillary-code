e7fee7c0efed89cc7edb2719f66867437ecf55eb
Thomas "moenk" Meier <moenk@geoclub.de>
2020-11-10 16:14:02 +0100

    +cv2.normalize

--- python3/jpegoptimizer3.py
+++ python3/jpegoptimizer3.py
@@ -1,10 +1,18 @@
+#
+#   file: jpegoptimizer3.py
+#
+#   coder: geomoenk@googlemail.com
+#
+#   purpose: optimize all JPG files with EXIF headers from a directory
+#            to 75% quality using OpenCV and Pillow before uploading
+#
+
 import os
 import sys
-from tqdm import tqdm
-from PIL import Image
-# from PIL import ImageOps
 import cv2
 import numpy as np
+from tqdm import tqdm
+from PIL import Image
 
 
 print ("*** JPEG optimizer ***")
@@ -20,15 +28,11 @@ for subdirz, dirz, filez in os.walk(ordnerpfad):
         file_path = subdirz + os.sep + f
         if file_path.lower().endswith('.jpg'):
             image_org = Image.open(file_path)
-            # try:
-            exifdata = image_org.info['exif']
-            # image_yuv = cv2.cvtColor(np.array(image_org), cv2.COLOR_RGB2YUV)
-            # image_yuv[:, :, 0] = cv2.equalizeHist(image_yuv[:, :, 0])
-            # image_rgb = Image.fromarray(cv2.cvtColor(image_yuv, cv2.COLOR_YUV2RGB))
-            image_bgr = cv2.cvtColor(np.array(image_org), cv2.COLOR_RGB2BGR)
-            image_bgr = cv2.normalize(image_bgr, None, 0, 255, cv2.NORM_MINMAX)
-            image_rgb = Image.fromarray(cv2.cvtColor(image_bgr, cv2.COLOR_BGR2RGB))
-            image_rgb.save(file_path, optimize=True, quality=75, exif=exifdata)
-        # except:
-        #         image_org.close()
-        #         os.remove(file_path)
+            try:
+                exifdata = image_org.info['exif']
+                image_cv2 = cv2.normalize(np.array(image_org), None, 0, 255, cv2.NORM_MINMAX)
+                image_rgb = Image.fromarray(image_cv2)
+                image_rgb.save(file_path, optimize=True, quality=75, exif=exifdata)
+            except:
+                image_org.close()
+                os.remove(file_path)
